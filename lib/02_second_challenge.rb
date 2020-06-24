56d4aca4f158ab7ff896c9c31da2fbb3c524d99e
Adewale Olusegun Ige <victorpowergen@gmail.com>
2020-06-24 07:55:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -7,8 +7,13 @@ def second_challenge
       :hero_friends => []
       }, 
     :capulet => {
-      #your key/value pairs here!
-      }}
+           :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    
+  }
 
   
 end
